b223b093d2af953927d37cc5eb3530a94049c5c8
ekerintaiwo <ekerinolufemo>
2022-04-04 17:13:39 +0200

    Basic styling using react-scroll npm package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div id="section1">
+        <h1>Section 1</h1>
+        Men of the Nigerian army recorded a great success in it’s war against insurgency in the Northeast region of the country.
+
+The troops while conducting clearance operation in Sabisa forest rescued two elderly victims from Boko Haram captivity.
+
+Salemgists gathered from military sources that the gallant troops also destroyed camps and logistics warehouses belonging to Boko Haram terrorists.
+
+One of the victim who was an elderly man revealed that he was abducted by the insurgents who forcefully held him hostage for a long time now.
+
+Another victim who is an elderly woman stated that she has been in Boko Haram captivity for more than eight years now.
+
+A military source further said the victims were rescued along with other girls. The source added that Nigerian troops are now occupying Sabisa forest.
+      </div>
+      <div id="section2">
+        <h1>Section 2</h1>
+        Men of the Nigerian army recorded a great success in it’s war against insurgency in the Northeast region of the country.
+
+The troops while conducting clearance operation in Sabisa forest rescued two elderly victims from Boko Haram captivity.
+
+Salemgists gathered from military sources that the gallant troops also destroyed camps and logistics warehouses belonging to Boko Haram terrorists.
+
+One of the victim who was an elderly man revealed that he was abducted by the insurgents who forcefully held him hostage for a long time now.
+
+Another victim who is an elderly woman stated that she has been in Boko Haram captivity for more than eight years now.
+
+A military source further said the victims were rescued along with other girls. The source added that Nigerian troops are now occupying Sabisa forest.
+      </div>
+      <div id="section3">
+        <h1>Section 3</h1>
+        Men of the Nigerian army recorded a great success in it’s war against insurgency in the Northeast region of the country.
+
+The troops while conducting clearance operation in Sabisa forest rescued two elderly victims from Boko Haram captivity.
+
+Salemgists gathered from military sources that the gallant troops also destroyed camps and logistics warehouses belonging to Boko Haram terrorists.
+
+One of the victim who was an elderly man revealed that he was abducted by the insurgents who forcefully held him hostage for a long time now.
+
+Another victim who is an elderly woman stated that she has been in Boko Haram captivity for more than eight years now.
+
+A military source further said the victims were rescued along with other girls. The source added that Nigerian troops are now occupying Sabisa forest.
+      </div>
+      <div id="section4">
+        <h1>Section 4</h1>
+        Men of the Nigerian army recorded a great success in it’s war against insurgency in the Northeast region of the country.
+
+The troops while conducting clearance operation in Sabisa forest rescued two elderly victims from Boko Haram captivity.
+
+Salemgists gathered from military sources that the gallant troops also destroyed camps and logistics warehouses belonging to Boko Haram terrorists.
+
+One of the victim who was an elderly man revealed that he was abducted by the insurgents who forcefully held him hostage for a long time now.
+
+Another victim who is an elderly woman stated that she has been in Boko Haram captivity for more than eight years now.
+
+A military source further said the victims were rescued along with other girls. The source added that Nigerian troops are now occupying Sabisa forest.
+      </div>
     </div>
   );
 }
